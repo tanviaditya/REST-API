1f8789d44780f41afc3e13fb3bf9461414cb984c
Tanvi Aditya <47853977+tanviaditya@users.noreply.github.com>
2021-03-01 15:32:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   secretKey: "12345-67890-12345-67890",
   mongoUrl: "mongodb://localhost:27017/conFusion",
   facebook: {
-    clientId: '658703388068713',
-    clientSecret: 'e014721fe511026b4a42e34755d69e7e',
+//     clientId: 'YOUR_ID',
+//     clientSecret: 'YOUR_SECRET',
   },
 };
